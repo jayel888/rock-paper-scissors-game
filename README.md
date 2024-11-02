754f429aad5a9b141a9ee19acee0007f95118a65
jayel888 <15644@coderacademy.edu.au>
2024-11-02 11:19:49 +1100

    feat: added score keeping feature

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Logic:
 - Generate a random computer choice
 - Compare the users choice to the computer choice
 - Determine the winner
+
+- Add a feature: keep score.
--- src/scripts/rockpaperscissors.js
+++ src/scripts/rockpaperscissors.js
@@ -3,6 +3,11 @@ const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 const resultDiv = document.getElementById("result");
+const scoreDiv = document.getElementById("score");
+
+// Initialise the two variables to be 0
+let userScore = 0;
+let computerScore = 0;
 
 // get the computer choice
 function getComputerChoice() {
@@ -14,33 +19,37 @@ function getComputerChoice() {
 
 // write the logic for the winner
 function determineWinner(userChoice, getComputerChoice){
-    if (userChoice === getComputerChoice){
+    if (userChoice === getComputerChoice()){
         return "It's a tie";
     } else if (
-        (userChoice === 'Rock' && getComputerChoice === 'Scissors') ||
-        (userChoice === 'Paper' && getComputerChoice === 'Rock') ||
-        (userChoice === 'Scissors' && getComputerChoice === 'Paper')
+        (userChoice === 'Rock' && getComputerChoice() === 'Scissors') ||
+        (userChoice === 'Paper' && getComputerChoice() === 'Rock') ||
+        (userChoice === 'Scissors' && getComputerChoice() === 'Paper')
     ) {
+        userScore++;
+        scoreDiv.textContent = `Your Score: ${userScore} | Computer Score: ${computerScore}`;
         return "You win!";
     } else {
+        computerScore++;
+        scoreDiv.textContent = `Your Score: ${userScore} | Computer Score: ${computerScore}`;
         return "Computer wins! :(";
     }
 }
 // listen to the button click events
 rockButton.addEventListener('click', () => {
-    const getComputerChoice = getComputerChoice()
+    const computerChoice = getComputerChoice()
     const result = determineWinner('Rock', getComputerChoice)
-    resultDiv.textContent = `You chose Rock. Computer chose ${getComputerChoice}. ${result}`;
+    resultDiv.textContent = `You chose Rock. Computer chose ${computerChoice}. ${result}`;
 })
 
 paperButton.addEventListener('click', () => {
-    const getComputerChoice = getComputerChoice()
+    const computerChoice = getComputerChoice()
     const result = determineWinner('Paper', getComputerChoice)
-    resultDiv.textContent = `You chose Paper. Computer chose ${getComputerChoice}. ${result}`;
+    resultDiv.textContent = `You chose Paper. Computer chose ${computerChoice}. ${result}`;
 })
 
 scissorsButton.addEventListener('click', () => {
-    const getComputerChoice = getComputerChoice()
+    const computerChoice = getComputerChoice()
     const result = determineWinner('Scissors', getComputerChoice)
-    resultDiv.textContent = `You chose Rock. Computer chose ${getComputerChoice}. ${result}`;
+    resultDiv.textContent = `You chose Rock. Computer chose ${computerChoice}. ${result}`;
 })
